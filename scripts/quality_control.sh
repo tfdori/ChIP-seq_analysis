c57b6d8bf0e12d8835005b945e5d87d31ca4a97a
Tfirst Dóra <tfirst.dora@gmail.com>
2025-11-19 19:00:39 +0100

    Update comments

--- scripts/quality_control.sh
+++ scripts/quality_control.sh
@@ -6,10 +6,10 @@ mkdir -p ${TARGET_DIR}
 
 INPUT_DIR="/home/jw0rtd/omics_data_analysis/ChIP_analysis/0000_data"
 
-# Define the file containin the input filename's prefix
+# File containing the prefix of the input file
 prefix_list="/home/jw0rtd/omics_data_analysis/ChIP_analysis/filename_prefix.txt"
 
-# Execute the quality control on each sample
+# Execute the quality control
 while read prefix
 do
 	fastqc -t 1 -o ${TARGET_DIR} ${INPUT_DIR}/${prefix}_R1.fastq.gz
